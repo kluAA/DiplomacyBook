b32be0342f7fe8901c75864b64e8f80a63eccf0f
kevinlu <kevinlu.rea@gmail.com>
2020-04-13 01:36:51 -0400

    Reduce n+1 for fetching photos

--- DiplomacyBook/app/controllers/api/photos_controller.rb
+++ DiplomacyBook/app/controllers/api/photos_controller.rb
@@ -1,7 +1,7 @@
 class Api::PhotosController < ApplicationController
     def show
         user = User.find(params[:id])
-        @post_photos = user.posts.joins(:photo_attachment)
+        @post_photos = user.posts.joins(:photo_attachment).with_attached_photo
         render :show
     end
 
